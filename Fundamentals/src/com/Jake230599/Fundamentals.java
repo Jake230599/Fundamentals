2caa94a0efaecaf20f242d46ef3efcba7d675a96
Jake230599 <jake.r0523@gmail.com>
2013-03-21 18:44:35 +1100

    First commit, initializing repository.

---
+++ Fundamentals/src/com/Jake230599/Fundamentals.java
@@ -0,0 +1,14 @@
+package com.Jake230599;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Fundamentals extends JavaPlugin {
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+
+}
